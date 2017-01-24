fedfe816c5852b52ae5d32dbd2127f83aa4aa0af
Janis Krasemann <janis.krasemann@enpit.de>
2017-01-24 09:35:43 +0100

    Upgrade API

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const pickpocket = require('pickpocket');
 const readBody = require('express-readbody');
 
 const server = express();
-const p = pickpocket();
 
 server.use((req, res, next) => {
   // Request methods you wish to allow
@@ -23,17 +22,23 @@ server.use((req, res, next) => {
 });
 
 server.get('/authorize', (req, res) =>
-  p.authorize()
+  pickpocket.authorize()
+    .then(auth => answer(res, 200, auth))
+    .catch(err => answer(res, 400, err))
+);
+
+server.get('/access-token/:token', (req, res) =>
+  pickpocket.getAccessToken(req.params.token)
     .then(auth => answer(res, 200, auth))
     .catch(err => answer(res, 400, err))
 );
 
 server.post('/archive', (req, res) =>
-  readBody(req).then(
-    options => p.archiveOverdueArticles(options).then(
-      articles => answer(res, 200, JSON.stringify(articles))
-    )
-  ).catch(err => answer(res, 400, err))
+  readBody(req)
+    .then(body => JSON.parse(body))
+    .then(options => pickpocket.archive(options.token, options))
+    .then(articles => answer(res, 200, JSON.stringify(articles)))
+    .catch(err => answer(res, 400, err))
 );
 
-server.listen(8081, () => process.write.stdout('pickpocket webservice listening at port 8081'));
+server.listen(8081, () => console.log('pickpocket webservice listening at port 8081'));
